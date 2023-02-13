5e81d1ff53ee767298f8fb61feb975ae124905bf
Marika Mäki <zeracheciecl@gmail.com>
2023-02-13 16:44:12 +0200

    Update 1 css

--- proman/views/nav.php
+++ proman/views/nav.php
@@ -1,9 +1,19 @@
 <nav>
     <ul>
-        <li><a href="/~e2101751/php/proman/">theTrackerApp</a></li>
-        <li><a href="/~e2101751/php/proman/controllers/project_list.php">Project list</a></li>
-        <li><a href="/~e2101751/php/proman/controllers/task_list.php">Task list</a></li>
-        <li><a href="/~e2101751/php/proman/controllers/project.php">Add project</a></li>
-        <li><a href="/~e2101751/php/proman/controllers/task.php">Add task</a></li>
+        <button class="dropbtn"><a href=/~e2101751/php/proman/>theTrackerApp</a></button>
+        <div class="dropdown">
+            <button class="dropbtn">Lists</button>
+            <div class="dropdown-content">
+        <a href="/~e2101751/php/proman/controllers/project_list.php">Project list</a>
+        <a href="/~e2101751/php/proman/controllers/task_list.php">Task list</a>
+        </div>
+</div>
+        <div class="dropdown">
+        <button class="dropbtn">Add New</button>
+        <div class="dropdown-content">
+        <a href="/~e2101751/php/proman/controllers/project.php">Add project</a>
+        <a href="/~e2101751/php/proman/controllers/task.php">Add task</a>
+</div>
+</div>
 </ul>
 </nav>
